95851aad3fcc864f7b9fcb6f6695b8db227ef825
kdnhyk <kdnhyk@gmail.com>
2023-05-26 16:46:08 +0900

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,18 +17,6 @@ export default function Document() {
         <link rel="apple-touch-icon" href="/logo192.png" />
 
         <meta
-          property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/sunys-1dcf2.appspot.com/o/og_image.png?alt=media&token=adaa046e-ec2d-466a-9e40-828afe0bee71"
-        />
-        <meta
-          property="og:description"
-          content="서니즈 | 브랜드 아카이브 매거진"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="SUNYS" />
-        <meta property="og:url" content="http://sunys.co.kr" />
-
-        <meta
           name="naver-site-verification"
           content="ad17f2a0cf5bc90c17029a9a38adc62de09b2ef0"
         />

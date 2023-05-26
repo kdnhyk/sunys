77b29bbcea870528d21420365bce6d8df0641a5b
kdnhyk <kdnhyk@gmail.com>
2023-05-26 17:53:23 +0900

    fix

--- src/pages/brand/index.tsx
+++ src/pages/brand/index.tsx
@@ -10,6 +10,18 @@ export default function BrandList() {
       <Head>
         <title>SUNYS | 전체 브랜드</title>
         <meta name="description" content="전체 브랜드 모음" />
+
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/sunys-1dcf2.appspot.com/o/og_image.png?alt=media&token=adaa046e-ec2d-466a-9e40-828afe0bee71"
+        />
+        <meta
+          property="og:description"
+          content="서니즈 | 브랜드 아카이브 매거진"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="SUNYS" />
+        <meta property="og:url" content="http://sunys.co.kr" />
       </Head>
       <BrandListStyle>
         <div className="BrandListArea">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,6 +50,18 @@ export default function News() {
       <Head>
         <title>SUNYS | 뉴스</title>
         <meta name="description" content="서니즈 | 브랜드 별 새로운 컬렉션" />
+
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/sunys-1dcf2.appspot.com/o/og_image.png?alt=media&token=adaa046e-ec2d-466a-9e40-828afe0bee71"
+        />
+        <meta
+          property="og:description"
+          content="서니즈 | 브랜드 아카이브 매거진"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="SUNYS" />
+        <meta property="og:url" content="http://sunys.co.kr" />
       </Head>
       <NewsWrap>
         <div className="NewColArea">
